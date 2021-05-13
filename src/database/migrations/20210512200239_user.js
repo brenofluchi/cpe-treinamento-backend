d2318d33a57b8f37f6d164986eac676509a90e27
Lucas Macedo <lucasguerra.macedo@gmail.com>
2021-05-13 17:18:43 -0300

    knex migrate:latest ("itens" do user)

--- src/database/migrations/20210512200239_user.js
+++ src/database/migrations/20210512200239_user.js
@@ -1,8 +1,19 @@
 
 exports.up = function(knex) {
+    return knex.schema.createTable("user", function (table){
+        table.string('user_id').primary().notNullable();
+        table.string('nome').notNullable();
+        table.string('email').notNullable();
+        table.string('telefone').notNullable();
+        table.string('estado').notNullable();
+        table.string('cidade').notNullable();
+        table.string('cep').notNullable();
+        table.string('logradouro').notNullable();
+        table.string('sonho').notNullable();
+    });
+  };
   
-};
-
-exports.down = function(knex) {
+  exports.down = function(knex) {
+      return knex.schema.dropTable("user");
+  };
   
-};
